9b82e3d4de6596eeaa3633213f07b014763897d3
Konrad Schultz <kschultz@Level48.com>
2016-10-25 22:38:29 -0700

    initial layout of worldTrotter

--- WorldTrotter/WorldTrotter/WorldTrotter/ViewController.swift
+++ WorldTrotter/WorldTrotter/WorldTrotter/ViewController.swift
@@ -9,21 +9,6 @@
 import UIKit
 
 class ViewController: UIViewController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        let boundingBox = CGRect(x: 160, y: 240, width: 100, height: 150)
-        let firstView = UIView(frame: boundingBox)
-        
-        let bBoxTwo = CGRect(x: 20, y: 30, width: 50, height: 50)
-        let secondView = UIView(frame: bBoxTwo)
-        
-        
-        
-        firstView.backgroundColor = UIColor.blue
-        secondView.backgroundColor = UIColor.gray
-        view.addSubview(firstView)
-        firstView.addSubview(secondView)
-    }
+
 }
 
